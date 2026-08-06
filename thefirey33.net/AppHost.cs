5c786848946dd21f993511f5c88946a1954d560a
thefirey33 <mmethefirey@gmail.com>
2026-08-07 01:11:24 +0300

    Update to include environment with domains.

--- thefirey33.net/AppHost.cs
+++ thefirey33.net/AppHost.cs
@@ -44,6 +44,7 @@ compose.ConfigureComposeFile(options =>
 });
 
 var zapret = builder.AddContainer("fireyfilteringbypass", "matecik/zapret")
+    .WithEnvironment("ZAPRET_DOMAINS", "gateway.discord.gg,discord.com")
     .PublishAsDockerComposeService((_, service) =>
     {
         service.Privileged = true;
